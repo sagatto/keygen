f0dfb396cbf1136f6f4b7165caae27c736db2165
sagatto <sagato@live.com>
2020-10-30 16:17:09 -0700

    readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
-
+function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
